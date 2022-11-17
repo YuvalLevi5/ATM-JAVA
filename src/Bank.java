41a160d07f6a81224c35380b9e36380e42a8807b
Yuval Vaknin Levi <yuvallevi0950@gmail.com>
2022-11-17 23:45:17 +0200

    user class

--- src/Bank.java
+++ src/Bank.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
 
 public class Bank {
+	public String name;
+	private ArrayList<User> user;
+	private ArrayList<Account> accounts;
 
 }
--- src/User.java
+++ src/User.java
@@ -1,4 +1,23 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 
 public class User {
+	private String firstName;
+	private String lastName;
+	private String uuid;
+	private byte pinHash[];
+	private ArrayList<Account> accounts;
 
+	public User(String firstName, String lastName, String pin, Bank theBank) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
 }
